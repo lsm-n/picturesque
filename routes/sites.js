23aa69fa391c5aeb7e75d56ba4a7cdcb05d1bf6f
L Nair <lsm.nair@gmail.com>
2017-01-10 06:28:39 +0000

    add ratings css

--- routes/sites.js
+++ routes/sites.js
@@ -75,6 +75,31 @@ router.get("/:id", function(req, res) {
 
 });
 
+// rate
+router.put("/:id/rate", middleware.isLoggedIn, function (req, res) {
+    var score = req.body.score;
+    
+    Site.findById(req.params.id, function(err, site) {
+        if (err) {
+            console.log(err);
+        } else {
+            site.scores.push(score);
+            
+            // avgs. new score into overall rating
+            var total = 0;
+            for (var i = 0; i < site.scores.length; i++) {
+               total = total + site.scores[i]; 
+            }
+            site.rating = total/site.scores.length;
+            site.save();
+            
+            req.flash("success", "Your rating of " + site.name + " has been saved");
+            res.redirect("/sites/" + req.params.id);
+            
+        }
+    });
+});
+
 // edit
 router.get("/:id/edit", middleware.checkSiteAuth, function (req, res) {
     Site.findById(req.params.id, function(err, site) {
@@ -99,31 +124,6 @@ router.put("/:id", middleware.checkSiteAuth, function (req, res) {
     });
 });
 
-// rate
-router.put("/:id/rate", middleware.isLoggedIn, function (req, res) {
-    var score = req.body.score;
-    
-    Site.findById(req.params.id, function(err, site) {
-        if (err) {
-            console.log(err);
-        } else {
-            site.scores.push(score);
-            
-            // avgs. new score into overall rating
-            var total = 0;
-            for (var i = 0; i < site.scores.length; i++) {
-               total = total + site.scores[i]; 
-            }
-            site.rating = total/site.scores.length;
-            
-            site.save();
-            
-            req.flash("success", "Your rating of " + site.name + " has been saved");
-            res.redirect("/sites/" + req.params.id);
-            
-        }
-    });
-});
 
 //delete a campground
 router.delete("/:id", middleware.checkSiteAuth, function (req, res) {
